62224c7a9016871796b0b3a27a87105106ab22a9
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2021-09-23 21:03:16 +0700

    OS212 chrisbagas TXT

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,21 +1,55 @@
-#! /bin/bash
+#!/bin/bash
+# Copyright (C) 2020-2021 Cicak Bin Kadal
 
-sha="SHA256SUM"
-files="my*.txt my*.sh"
+# This free document is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-echo "Remove SHA files"
-rm -f $sha $sha.asc
+# REV02 Sun 19 Sep 2021 15:44:11 WIB
+# REV01 Sun 12 Sep 2021 03:10:00 WIB
+# START Mon 28 Sep 21:05:04 WIB 2020
 
-echo "Make SHA256SUM of files"
-sha256sum $files > $sha
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
 
-echo "Verify files have not change"
-sha256sum -c $sha
+REC2="christopher.bagas@ui.ac.id"
+REC1="operatingsystems@vlsm.org"
+FILES="my*.asc my*.txt my*.sh"
+SHA="SHA256SUM"
 
-echo "Make a detached signature for the SHA256SUM file in SHA256SUM.asc"
-gpg --output $sha.asc --armor --sign --detach-sign $sha
+[ -d $HOME/RESULT/ ] || mkdir -p $HOME/RESULT/
+pushd $HOME/RESULT/
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -f $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
 
-echo "Verify SHA256SUM integrity with SHA256SUM.asc signature"
-gpg --verify $sha.asc $sha
+for II in $HOME/RESULT/myW*.tar.bz2.asc ; do
+   echo "Check and move $II..."
+   [ -f $II ] && mv -f $II .
+done
+
+echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
+gpg --output $SHA.asc --armor --sign --detach-sign $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
 
 exit 0
